dd2f6d87e384c6b325492289d7fee2682715af29
Nguyen Hoang Lam <lam98iu@gmail.com>
2024-08-02 10:20:57 +0700

    test

--- app.py
+++ app.py
@@ -3,3 +3,11 @@ import pandas as pd
 
 st.title("Excel File Reader, Hoang Lam")
 st.markdown("_Prototype v0.4.1_")
+
+@st.cache_data
+def load_data(path:str):
+    data = pd.read_excel(path)
+    return data
+
+df = load_data("./data.xlsx")
+st.dataframe(df)
